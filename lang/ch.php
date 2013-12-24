33fe07cf728c90156ee83cc4ae47dc0f06552354
gary531 <weiganyi@yeah.net>
2013-12-24 17:53:01 +0800

    modify a category title text for ch lang

--- lang/ch.php
+++ lang/ch.php
@@ -7,23 +7,23 @@ $g_lang_text = array();
 
 /* login.php */
 $g_lang_text["login_page_title"] = "登录";
-$g_lang_text["login_user_name"] = "用户名";
+$g_lang_text["login_user_name"] = "用户�?;
 $g_lang_text["login_user_passwd"] = "密码";
 $g_lang_text["login_submit"] = "登录";
 $g_lang_text["login_goto_register"] = "我要注册一个新帐户!";
 $g_lang_text["login_user_null"] = "提示：用户名为空.";
-$g_lang_text["login_passwd_null"] = "提示：密码为空.";
+$g_lang_text["login_passwd_null"] = "提示：密码为�?";
 
 /* register.php */
 $g_lang_text["reg_page_title"] = "注册";
-$g_lang_text["reg_user_name"] = "用户名";
+$g_lang_text["reg_user_name"] = "用户�?;
 $g_lang_text["reg_user_passwd"] = "密码";
 $g_lang_text["reg_user_mail"] = "邮箱";
 $g_lang_text["reg_submit"] = "注册";
-$g_lang_text["reg_goto_login"] = "我已有一个帐户，去登录!";
+$g_lang_text["reg_goto_login"] = "我已有一个帐户，去登�?";
 $g_lang_text["reg_user_null"] = "提示：用户名为空.";
-$g_lang_text["reg_passwd_null"] = "提示：密码为空.";
-$g_lang_text["reg_email_null"] = "提示：邮箱为空.";
+$g_lang_text["reg_passwd_null"] = "提示：密码为�?";
+$g_lang_text["reg_email_null"] = "提示：邮箱为�?";
 
 /* tb_login.php */
 $g_lang_text["tb_login_passwd_incorrent"] = "提示：密码不正确.";
@@ -33,7 +33,7 @@ $g_lang_text["tb_login_server_stop"] = "提示：登录服务已暂停.";
 
 /* menu.php */
 $g_lang_text["menu_home"] = "浏览博客";
-$g_lang_text["menu_new_post"] = "写文章";
+$g_lang_text["menu_new_post"] = "写文�?;
 $g_lang_text["menu_managment"] = "管理博客";
 $g_lang_text["menu_admin_post"] = "文章管理";
 $g_lang_text["menu_admin_category"] = "分类管理";
@@ -41,15 +41,15 @@ $g_lang_text["menu_admin_comment"] = "评论管理";
 $g_lang_text["menu_admin_config"] = "博客配置";
 $g_lang_text["menu_admin_login"] = "登录";
 $g_lang_text["menu_admin_register"] = "注册";
-$g_lang_text["menu_admin_logout"] = "退出登录";
+$g_lang_text["menu_admin_logout"] = "退出登�?;
 
 /* sidebar.php */
 $g_lang_text["sidebar_notice_title"] = "公告";
-$g_lang_text["sidebar_summary_title"] = "信息汇总";
-$g_lang_text["sidebar_category_title"] = "公告";
+$g_lang_text["sidebar_summary_title"] = "信息汇�?;
+$g_lang_text["sidebar_category_title"] = "分类";
 $g_lang_text["sidebar_archive_title"] = "归档";
 $g_lang_text["sidebar_reading_title"] = "阅读排行";
-$g_lang_text["sidebar_commnet_title"] = "最新评论";
+$g_lang_text["sidebar_commnet_title"] = "最新评�?;
 $g_lang_text["sidebar_summary_bloger"] = "博主: ";
 $g_lang_text["sidebar_summary_register"] = "注册时间: ";
 $g_lang_text["sidebar_summary_posts"] = "文章数量: ";
@@ -58,22 +58,22 @@ $g_lang_text["sidebar_summary_comment"] = "评论数量: ";
 $g_lang_text["sidebar_summary_email"] = "邮箱: ";
 
 /* tb_function.php */
-$g_lang_text["tb_func_uncategorized"] = "未分类";
+$g_lang_text["tb_func_uncategorized"] = "未分�?;
 $g_lang_text["tb_func_post_read"] = "阅读";
 $g_lang_text["tb_func_post_comment"] = "评论";
 $g_lang_text["tb_func_post_edit"] = "编辑";
 $g_lang_text["tb_func_post_delete"] = "删除";
 $g_lang_text["tb_func_comment_delete"] = "删除";
-$g_lang_text["tb_func_comment_user"] = "用户名";
+$g_lang_text["tb_func_comment_user"] = "用户�?;
 $g_lang_text["tb_func_comment_submit"] = "提交";
 
 /* post.php */
 $g_lang_text["post_view_head"] = "文章浏览";
 $g_lang_text["post_comment_view_head"] = "评论浏览";
-$g_lang_text["post_comment_write_head"] = "写评论";
+$g_lang_text["post_comment_write_head"] = "写评�?;
 
 /* admin_category.php */
-$g_lang_text["admin_cat_head_cat"] = "分类名";
+$g_lang_text["admin_cat_head_cat"] = "分类�?;
 $g_lang_text["admin_cat_head_edit"] = "新分类名";
 $g_lang_text["admin_cat_head_op"] = "操作";
 $g_lang_text["admin_cat_edit"] = "编辑";
@@ -83,19 +83,19 @@ $g_lang_text["admin_cat_submit"] = "提交";
 
 /* admin_comment.php */
 $g_lang_text["admin_comment_head_content"] = "评论内容";
-$g_lang_text["admin_comment_head_user"] = "用户名";
+$g_lang_text["admin_comment_head_user"] = "用户�?;
 $g_lang_text["admin_comment_head_date"] = "日期";
 $g_lang_text["admin_comment_head_op"] = "操作";
 $g_lang_text["admin_comment_delete"] = "删除";
 
 /* admin_config.php */
-$g_lang_text["admin_config_password"] = "新密码";
+$g_lang_text["admin_config_password"] = "新密�?;
 $g_lang_text["admin_config_email"] = "新邮箱地址";
 $g_lang_text["admin_config_language"] = "语言";
-$g_lang_text["admin_config_blog_name"] = "博客名";
+$g_lang_text["admin_config_blog_name"] = "博客�?;
 $g_lang_text["admin_config_blog_notice"] = "博客公告";
-$g_lang_text["admin_config_bloger_name"] = "博主名";
-$g_lang_text["admin_config_page_posts"] = "每页显示文章数";
+$g_lang_text["admin_config_bloger_name"] = "博主�?;
+$g_lang_text["admin_config_page_posts"] = "每页显示文章�?;
 $g_lang_text["admin_config_foot_text"] = "页脚描述";
 $g_lang_text["admin_config_submit"] = "提交";
 
